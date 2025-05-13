255a8768b14f33e10720eff4a2e201374712856d
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-05-13 16:23:48 +0600

    [backend] Реализован WebSocket-сервера для рисования

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
+import WebSocket from "ws";
 
 const app = express();
 const wsInstance = expressWs(app);
@@ -11,8 +12,37 @@ app.use(cors());
 const router = express.Router();
 wsInstance.applyTo(router);
 
+const connectedClient: WebSocket[] = [];
+
 router.ws('/canvas', (ws, req) => {
-    console.log('client connected');
+    connectedClient.push(ws);
+
+    interface IncomingPositions {
+        type: string;
+        payload: string;
+    }
+
+    ws.on('message', (positionsArr) => {
+        try {
+            const decodedMessage = JSON.parse(positionsArr.toString()) as IncomingPositions;
+
+            if (decodedMessage.type === "DRAW") {
+                connectedClient.forEach((clientWS) => {
+                    clientWS.send(JSON.stringify({
+                        type: "DRAW",
+                        payload: decodedMessage.payload,
+                    }));
+                })
+            }
+        } catch (e) {
+            ws.send(JSON.stringify({error: "Invalid message format"}));
+        }
+    })
+
+    ws.on('close', () => {
+        const index = connectedClient.indexOf(ws);
+        connectedClient.splice(index, 1);
+    })
 });
 
 app.use(router);
